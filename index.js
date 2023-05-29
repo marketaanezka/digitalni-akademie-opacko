02f2227092d6c5c5d2ce893eb7dcdc58a934b23d
Marketa Willis <marketa.willis@applifting.cz>
2023-05-29 18:35:01 +0200

    add bonus

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const toggleSwitch = document.querySelector(
   '.theme-switch input[type="checkbox"]',
 );
 
-const switchTheme = (e) => {
+function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'dark'); //add this
   }
-};
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light'); //add this
+  }
+}
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
 
 toggleSwitch.addEventListener('change', switchTheme, false);
